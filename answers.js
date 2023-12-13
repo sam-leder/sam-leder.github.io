5687ebdf74272a674ad078fbdd3a9ca8874ae711
sam-leder <153715541+sam-leder@users.noreply.github.com>
2023-12-13 11:02:43 +0100

    Create answers.js

---
+++ answers.js
@@ -0,0 +1,5 @@
+  const benchmarkSchemes = [
+    { name: 'Kyber', answers: ['yes', 'no', 'yes', 'Kyber', 'yes'] },
+    { name: 'McEliece', answers: ['yes', 'yes', 'no', 'RSA', 'yes'] },
+    { name: 'FrodoKEM', answers: ['no', 'yes', 'yes', "I don't know", 'no'] },
+  ];
