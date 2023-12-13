024b2987d2a446d3e94f23f58c8d39d1706db436
sam-leder <153715541+sam-leder@users.noreply.github.com>
2023-12-13 11:38:31 +0100

    Update and rename answers.js to questions.js

--- answers.js
+++
@@ -1,5 +0,0 @@
-  const benchmarkSchemes = [
-    { name: 'Kyber', answers: ['yes', 'no', 'yes', 'Kyber', 'yes'] },
-    { name: 'McEliece', answers: ['yes', 'yes', 'no', 'RSA', 'yes'] },
-    { name: 'FrodoKEM', answers: ['no', 'yes', 'yes', "I don't know", 'no'] },
-  ];
